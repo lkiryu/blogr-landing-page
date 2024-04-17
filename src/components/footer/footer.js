7fcf2b8b36295c3425e9a1df539e1b9e9bb5d645
kiryu <13matheusilva@gmail.com>
2024-04-17 17:36:16 -0300

    footer style add

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,38 +1,42 @@
 import React from "react";
 import logo from '../../images/logo.svg'
 
+import './footer.css'
+
 const Footer = () => {
     return (
         <footer className="footer">
-            <a href="/">
-                <img src={logo} alt="logo"></img>
-            </a>
-            <div>
-                <h2 className="title">Product</h2>
-                <ul className="links">
-                    <li><a href="/">Overview</a></li>
-                    <li><a href="/">Pricing</a></li>
-                    <li><a href="/">Marketplace</a></li>
-                    <li><a href="/">Features</a></li>
-                    <li><a href="/">Integrations</a></li>
-                </ul>
-            </div>
-            <div>
-                <h2 className="title">Company</h2>
-                <ul className="links">
-                    <li><a href="/">About</a></li>
-                    <li><a href="/">Team</a></li>
-                    <li><a href="/">Blog</a></li>
-                    <li><a href="/">Careers</a></li>
-                </ul>
-            </div>
-            <div>
-                <h2 className="title">Connect</h2>
-                <ul className="links">
-                    <li><a href="/">Contact</a></li>
-                    <li><a href="/">Newsletter</a></li>
-                    <li><a href="/">LinkedIn</a></li>
-                </ul>
+            <div className="footer-content">
+                <a href="/">
+                    <img src={logo} alt="logo"></img>
+                </a>
+                <div>
+                    <p className="title">Product</p>
+                    <ul className="links">
+                        <li><a href="/">Overview</a></li>
+                        <li><a href="/">Pricing</a></li>
+                        <li><a href="/">Marketplace</a></li>
+                        <li><a href="/">Features</a></li>
+                        <li><a href="/">Integrations</a></li>
+                    </ul>
+                </div>
+                <div>
+                    <p className="title">Company</p>
+                    <ul className="links">
+                        <li><a href="/">About</a></li>
+                        <li><a href="/">Team</a></li>
+                        <li><a href="/">Blog</a></li>
+                        <li><a href="/">Careers</a></li>
+                    </ul>
+                </div>
+                <div>
+                    <p className="title">Connect</p>
+                    <ul className="links">
+                        <li><a href="/">Contact</a></li>
+                        <li><a href="/">Newsletter</a></li>
+                        <li><a href="/">LinkedIn</a></li>
+                    </ul>
+                </div>
             </div>
         </footer>
     )
