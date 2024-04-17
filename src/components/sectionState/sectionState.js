d400b1698323723dd61747f2a10d5116b82d8dbc
kiryu <13matheusilva@gmail.com>
2024-04-17 16:54:22 -0300

    section state component add

--- src/components/sectionState/sectionState.js
+++ src/components/sectionState/sectionState.js
@@ -1,15 +1,19 @@
 import React from "react";
 import phones from '../../images/illustration-phones.svg'
 
+import './sectionState.css'
+
 const SectionState = () => {
     return (
         <section className="state">
-            <img src={phones} alt="phones"></img>
+            <div className="state-content">
+                <img src={phones} alt="phones"></img>
 
-            <div className="info">
-                <h2 className="title">State of the Art Infrastructure</h2>
-                <p className="description">With reliability and speed in mind, worldwide data centers provide the backbone for ultra-fast connectivity.
-                    This ensures your site will load instantly, no matter where your readers are, keeping your site competitive.</p>
+                <div className="info">
+                    <h2 className="title">State of the Art Infrastructure</h2>
+                    <p className="description">With reliability and speed in mind, worldwide data centers provide the backbone for ultra-fast connectivity.
+                        This ensures your site will load instantly, no matter where your readers are, keeping your site competitive.</p>
+                </div>
             </div>
         </section>
     )
