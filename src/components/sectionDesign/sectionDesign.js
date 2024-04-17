260dea6f35db818c62eee62cf4a313ec02e207c2
kiryu <13matheusilva@gmail.com>
2024-04-17 16:37:17 -0300

    section design style add

--- src/components/sectionDesign/sectionDesign.js
+++ src/components/sectionDesign/sectionDesign.js
@@ -1,20 +1,24 @@
 import React from "react";
 
+import './sectionDesign.css'
+
 const SectionDesign = () => {
     return (
-        <section className="Design">
+        <section className="design">
             <h2 className="title">Designed for the future</h2>
-            <div className="info">
-                <h3 className="title">Introducing an extensible editor</h3>
-                <p className="description">Blogr features an exceedingly intuitive interface which lets you focus on one thing: creating content.
-                    The editor supports management of multiple blogs and allows easy manipulation of embeds such as images,
-                    videos, and Markdown. Extensibility with plugins and themes provide easy ways to add functionality or
-                    change the looks of a blog.</p>
-            </div>
-            <div className="info">
-                <h3 className="title">Robust content management</h3>
-                <p className="description">Flexible content management enables users to easily move through posts. Increase the usability of your blog
-                    by adding customized categories, sections, format, or flow. With this functionality, you’re in full control.</p>
+            <div className="info-content">
+                <div className="info">
+                    <h3 className="info-title">Introducing an extensible editor</h3>
+                    <p className="description">Blogr features an exceedingly intuitive interface which lets you focus on one thing: creating content.
+                        The editor supports management of multiple blogs and allows easy manipulation of embeds such as images,
+                        videos, and Markdown. Extensibility with plugins and themes provide easy ways to add functionality or
+                        change the looks of a blog.</p>
+                </div>
+                <div className="info">
+                    <h3 className="info-title">Robust content management</h3>
+                    <p className="description">Flexible content management enables users to easily move through posts. Increase the usability of your blog
+                        by adding customized categories, sections, format, or flow. With this functionality, you’re in full control.</p>
+                </div>
             </div>
         </section>
     )
