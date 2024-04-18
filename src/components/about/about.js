a5313a57caa870f6aadfc457cd62ec908d9befad
kiryu <13matheusilva@gmail.com>
2024-04-18 12:07:17 -0300

    desktop design changed

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -1,21 +1,25 @@
 import React from "react";
+import illustrationLaptop from '../../images/illustration-laptop-desktop.svg'
 
 import './about.css'
 
 const About = () => {
-    return(
+    return (
         <section className="about">
             <div className="about-content">
-                <div className="info">
-                    <h2 className="title">Free, open, simple</h2>
-                    <p className="description">Blogr is a free and open source application backed by a large community of helpful developers. It supports
-                        features such as code syntax highlighting, RSS feeds, social media integration, third-party commenting tools,
-                        and works seamlessly with Google Analytics. The architecture is clean and is relatively easy to learn.</p>
-                </div>
-                <div className="info">
-                    <h2 className="title">Powerful tooling</h2>
-                    <p className="description">Batteries included. We built a simple and straightforward CLI tool that makes customization and deployment a breeze, but
-                        capable of producing even the most complicated sites.</p>
+                <img src={illustrationLaptop} alt="illustration-laptop"></img>
+                <div className="infos">
+                    <div className="info">
+                        <h2 className="title">Free, open, simple</h2>
+                        <p className="description">Blogr is a free and open source application backed by a large community of helpful developers. It supports
+                            features such as code syntax highlighting, RSS feeds, social media integration, third-party commenting tools,
+                            and works seamlessly with Google Analytics. The architecture is clean and is relatively easy to learn.</p>
+                    </div>
+                    <div className="info">
+                        <h2 className="title">Powerful tooling</h2>
+                        <p className="description">Batteries included. We built a simple and straightforward CLI tool that makes customization and deployment a breeze, but
+                            capable of producing even the most complicated sites.</p>
+                    </div>
                 </div>
             </div>
         </section>
--- src/components/sectionDesign/sectionDesign.js
+++ src/components/sectionDesign/sectionDesign.js
@@ -1,4 +1,5 @@
 import React from "react";
+import illustrationEditor from '../../images/illustration-editor-desktop.svg'
 
 import './sectionDesign.css'
 
@@ -7,18 +8,21 @@ const SectionDesign = () => {
         <section className="design">
             <h2 className="title">Designed for the future</h2>
             <div className="info-content">
-                <div className="info">
-                    <h3 className="info-title">Introducing an extensible editor</h3>
-                    <p className="description">Blogr features an exceedingly intuitive interface which lets you focus on one thing: creating content.
-                        The editor supports management of multiple blogs and allows easy manipulation of embeds such as images,
-                        videos, and Markdown. Extensibility with plugins and themes provide easy ways to add functionality or
-                        change the looks of a blog.</p>
+                <div className="infos">
+                    <div className="info">
+                        <h3 className="info-title">Introducing an extensible editor</h3>
+                        <p className="description">Blogr features an exceedingly intuitive interface which lets you focus on one thing: creating content.
+                            The editor supports management of multiple blogs and allows easy manipulation of embeds such as images,
+                            videos, and Markdown. Extensibility with plugins and themes provide easy ways to add functionality or
+                            change the looks of a blog.</p>
+                    </div>
+                    <div className="info">
+                        <h3 className="info-title">Robust content management</h3>
+                        <p className="description">Flexible content management enables users to easily move through posts. Increase the usability of your blog
+                            by adding customized categories, sections, format, or flow. With this functionality, you’re in full control.</p>
+                    </div>
                 </div>
-                <div className="info">
-                    <h3 className="info-title">Robust content management</h3>
-                    <p className="description">Flexible content management enables users to easily move through posts. Increase the usability of your blog
-                        by adding customized categories, sections, format, or flow. With this functionality, you’re in full control.</p>
-                </div>
+                <img src={illustrationEditor} alt="illustration editor"></img>
             </div>
         </section>
     )
